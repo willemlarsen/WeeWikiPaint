07767a4fcdec77708a855d5a25454272a88e82fb
Willem Larsen <willemlarsen@gmail.com>
2014-06-30 15:09:55 -0700

    initial support for linting

--- Jakefile.js
+++ Jakefile.js
@@ -1,17 +1,7 @@
-task("default", [], function() {
-  console.log("default");
-});
-task("default", [], function() {
-	console.log("default");
-});
-
-desc ("Example!");
-task("example", ["dependency"], function () {
-	console.log("example task");
-
-});
-
-task("dependency", function() {
-	console.log("dependency");
-
+task("default", ["lint"]);
+	
+desc("Lint everything");
+task("lint", [], function() {
+  var lint = require("./build/lint/lint_runner.js");
+  lint.validateFile("Jakefile.js", {}, {});
 });
